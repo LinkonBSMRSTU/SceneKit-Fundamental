0d5dd8d2d6fa278cc0889b6d14c90f80045e492e
Fazle Rabbi Linkon <fazle.linkon@dreamonline.co.jp>
2021-01-21 16:27:24 +0600

    - Created Basic Cube, Light and Plane

--- SceneKit-Fundamental/ViewController.swift
+++ SceneKit-Fundamental/ViewController.swift
@@ -7,14 +7,65 @@
 //
 
 import UIKit
+import SceneKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let sceneView = SCNView(frame: self.view.frame)
+        self.view.addSubview(sceneView)
+        
+        let scene = SCNScene()
+        sceneView.scene = scene
+        
+        let camera = SCNCamera()
+        let cameraNode = SCNNode()
+        cameraNode.camera = camera
+        cameraNode.position = SCNVector3(-2.0, 4.0, 4.0)
+        
+        let ambientLight = SCNLight()
+        ambientLight.type = SCNLight.LightType.ambient
+        ambientLight.color = UIColor(red: 0.2, green: 0.2, blue: 0.2, alpha: 1.0)
+        cameraNode.light = ambientLight
+        
+        let light = SCNLight()
+        light.type = SCNLight.LightType.spot
+        light.spotInnerAngle = 30.0
+        light.spotOuterAngle = 80.0
+        light.castsShadow = true
+        
+        let lightNode = SCNNode()
+        lightNode.light = light
+        lightNode.position = SCNVector3(1.5, 1.5, 1.5)
+        
+        let cubeGeometry = SCNBox(width: 1.0, height: 1.0, length: 1.0, chamferRadius: 0.0)
+        let cubeNode = SCNNode(geometry: cubeGeometry)
+        
+        let planeGeometry = SCNPlane(width: 50.0, height: 50.0)
+        let planeNode = SCNNode(geometry: planeGeometry)
+        planeNode.eulerAngles = SCNVector3(x: GLKMathDegreesToRadians(-90), y: 0, z: 0)
+        planeNode.position = SCNVector3(x: 0, y: -0.5, z: 0)
+        
+        let redMaterial = SCNMaterial()
+        redMaterial.diffuse.contents = UIColor.red
+        cubeGeometry.materials = [redMaterial]
+         
+        let greenMaterial = SCNMaterial()
+        greenMaterial.diffuse.contents = UIColor.green
+        planeGeometry.materials = [greenMaterial]
+        
+        let constraint = SCNLookAtConstraint(target: cubeNode)
+        constraint.isGimbalLockEnabled = true
+        cameraNode.constraints = [constraint]
+        lightNode.constraints = [constraint]
+        
+        scene.rootNode.addChildNode(lightNode)
+        scene.rootNode.addChildNode(cameraNode)
+        scene.rootNode.addChildNode(cubeNode)
+        scene.rootNode.addChildNode(planeNode)
     }
-
-
 }
 
